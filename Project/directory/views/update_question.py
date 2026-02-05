8a3c3cfdb6f0e3a8a9598f1d6c4cd7dfd723b993
Plume <paulerichbauer@gmail.com>
2026-02-05 19:28:56 +0100

    Adding modification of answers in update_question

--- Project/directory/views/update_question.py
+++ Project/directory/views/update_question.py
@@ -6,6 +6,8 @@ from directory.models import Question, Answer
 from directory.forms import QuestionForm
 from directory.decorators import question_creator
 
+from django.forms import modelformset_factory
+
 
 @login_required
 @question_creator
@@ -20,24 +22,39 @@ def update_question(request, question_id):
 	)
 	
 	
+	AnswerFormSet = modelformset_factory(Answer, fields=['answer', 'is_correct'], can_delete=True, extra=1)
 	
 	if request.method == 'POST':
 		
 		update_question = QuestionForm(request.POST, instance=question)
+		answer_formset = AnswerFormSet(request.POST, queryset=answers)
 		
-		
-		if update_question.is_valid():
+		if update_question.is_valid() and answer_formset.is_valid():
 			
+			# Updating question
 			update_instance = update_question.save(commit=False)
 			update_instance.uploader = request.user
 			update_instance.save()
+			
+			
+			# Updating answers
+			submitted_answer_formset = answer_formset.save(commit=False)
+			for element in submitted_answer_formset:
+				element.question_linked = question
+				element.save()
+			#for
+			
+			# If answers are deleted
+			for element in answer_formset.deleted_objects:
+				element.delete()
+			#for
 			
 			messages.success(request, "The MCQ was updated with success!", extra_tags="success")
 			return redirect('single_question', question_id)
 		#if
 		
 		else:
-			messages.error(request, update_question.errors, extra_tags="danger")
+			messages.error(request, answer_formset.errors, extra_tags="danger")
 			return redirect('update_question', question_id)
 		#else
 		
@@ -46,6 +63,7 @@ def update_question(request, question_id):
 	
 	else:
 		update_question = QuestionForm(instance=question)
+		answer_formset = AnswerFormSet(queryset=answers)
 	#else
 	
 	
@@ -54,6 +72,7 @@ def update_question(request, question_id):
 		"question": question,
 		"answers": answers,
 		"update_question": update_question,
+		"answer_formset": answer_formset,
 	}
 	
 	return render(request, template_name, context)
